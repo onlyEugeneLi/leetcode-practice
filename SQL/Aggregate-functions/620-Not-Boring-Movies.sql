1e7d9221b33556e45cd984732a01304f43321757
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-02-24 14:12:33 +0000

    Add a NOT LIKE method

--- SQL/Aggregate-functions/620-Not-Boring-Movies.sql
+++ SQL/Aggregate-functions/620-Not-Boring-Movies.sql
@@ -20,10 +20,19 @@
 -- +----+------------+-------------+--------+
 
 ---------------------------------------------
----               Query                   ---
+---               Query 1                 ---
 ---------------------------------------------
 
 SELECT *
 FROM Cinema
 WHERE id % 2 = 1 AND description != 'boring'
+ORDER BY rating DESC;
+
+---------------------------------------------
+---               Query 2                 ---
+---------------------------------------------
+
+SELECT *
+FROM Cinema
+WHERE id % 2 = 1 AND description NOT LIKE '%boring%'
 ORDER BY rating DESC;
