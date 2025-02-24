9e9e66c2ac1db4baae7fc7e005ba0e2cb7b81f61
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-02-24 12:13:00 +0000

    Counter method

--- Hash-Map-Set/2352-Equal-Row-and-Column-Pairs.py
+++ Hash-Map-Set/2352-Equal-Row-and-Column-Pairs.py
@@ -1,12 +1,9 @@
 from collections import defaultdict, Counter
 class Solution:
     def equalPairs(self, grid: list[list[int]]) -> int:
-        occur_table = defaultdict(int)
+        # Generate a count dict automatically
+        occur_table = Counter(tuple(row) for row in grid)
         count = 0
-        # Counting occurence of each row
-        # Use Tuple as key of dict
-        for row in grid:
-            occur_table[tuple(row)] += 1
 
         # *: unpacks the 2D list to 3 lists (3 rows)
         # zip: collate all elements at the same index -> column
