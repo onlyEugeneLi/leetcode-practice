922e83afc430198311c7414ef6fa50072aecc557
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-04-02 10:10:42 +0100

    Update comments and understand solution

--- Linked-List/328-Odd-Even-Linked-List.py
+++ Linked-List/328-Odd-Even-Linked-List.py
@@ -11,25 +11,31 @@ from typing import Optional
 
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Null or sinlge case
+
+        """
+        Traverse list to change attribute [next]
+        """
+
+        # Base case: Null or sinlge case
         if not head or not head.next: 
             return head
-        odd = ListNode(0)
-        odd_ptr = odd
-        even = ListNode(0)
-        even_ptr = even
+        # Initialisation
+        odd = ListNode(0) # Dummy odd head (temporary var)
+        odd_ptr = odd # Set up odd pointer
+        even = ListNode(0) # Dummy even head (temporary var)
+        even_ptr = even # Set up even pointer
         idx = 1
 
         while head:
-            if idx % 2 == 0:
+            if idx % 2 == 0: # Detect odd / even
                 even_ptr.next = head
-                even_ptr = even_ptr.next
+                even_ptr = even_ptr.next # Move pointer to the tail to prepare for new node
             else:
                 odd_ptr.next = head
-                odd_ptr = odd_ptr.next
-            head = head.next
+                odd_ptr = odd_ptr.next # Move pointer to the tail to prepare for new node
+            head = head.next # Move on to next node in the Linked List
             idx += 1
         
-        even_ptr.next = None
-        odd_ptr.next = even.next
-        return odd.next
+        even_ptr.next = None # Set as the tail of the whole list
+        odd_ptr.next = even.next # Connect Odd list and Even list
+        return odd.next # Return head of list
