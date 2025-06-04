eb476a6f05f367105dceddeb7bad9648c9aeb6c9
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-06-04 15:16:41 +0100

    Add solution with better performance

--- Heap-Priority_Queue/215-Kth-Largest-Element-in-an-Array.py
+++ Heap-Priority_Queue/215-Kth-Largest-Element-in-an-Array.py
@@ -5,14 +5,17 @@ class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         # k-th Largest -- 从小到大，倒数第k的数
 
-        min_heap = []
+        # Take first k numbers in the list
+        heap = nums[:k]
 
-        for num in nums:
-            # heappush: makes sure the smallest element is always at the root (index 0)
-            heapq.heappush(min_heap, num)
-
-            # 
-            if len(min_heap) > k:
-                heapq.heappop(min_heap)
+        # Convert the k-length list into Heap
+        heapq.heapify(heap)
         
-        return min_heap[0]
+        # Start comparing from the index k number
+        for num in nums[k:]:
+            # Replace the smallest number in the heap with next larger number
+            if num > heap[0]:
+                heapq.heappop(heap)
+                heapq.heappush(heap, num)
+        
+        return heap[0]
