2c8d74554beb2ee9dbd480e10e5a92d59fb68541
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-06-14 21:05:24 +0100

    Update comment

--- Bit-Manipulation/1318-Minimum-Flips-to-Make-a-OR-b-Equal-to-c.py
+++ Bit-Manipulation/1318-Minimum-Flips-to-Make-a-OR-b-Equal-to-c.py
@@ -1,5 +1,12 @@
 class Solution:
     def minFlips(self, a: int, b: int, c: int) -> int:
+        '''
+        Initialisation
+
+        ab: current result
+        xor: the different bits between current result and c
+        ans: answer to return
+        '''
         ab, xor, ans = a | b, (a | b) ^ c, 0
         # Intuition:
         # 1. 初始的a | b结果跟c对比，看哪里不同
@@ -10,11 +17,22 @@ class Solution:
         # xor若为0，则为相同，否则有不同之处变为1
         # mask? 怎么按位迭代更新
         # Iterate through 30 bits
-        for i in range(31): # 2^30 > 10^9 题目限制数最大值10^9
-            mask = 1 << i
+        for i in range(31): # 2^30 > 10^9 (2^10 = 10^3) 题目限制数最大值10^9
+            mask = 1 << i # Used to detect 1 on the i-th bit
             if xor & mask > 0: # 看xor和mask是否相同
                 # 对比后发现如果a和b该位相同，则两个位都要改
+                # c & mask == 0: Check if c's i-th bit is 0
                 # 否则只有一个位要改
+                '''
+                xor & mask > 0
+                - difference between current result & c exists on the i-th bit (One of them has 1 and the other has 0)
+
+                (a & mask) == (b & mask) and (c & mask) == 0
+                - a and b has the same digit on the i-th bit
+                - difference confirmed, and c is 0 on i-th bit: then a and b must be both 1 on the i-th bit
+                - need to flip twice to change the result 
+                - Otherwise just once
+                '''
                 ans += 2 if (a & mask) == (b & mask) and (c & mask) == 0 else 1
             
-            return ans
+        return ans
