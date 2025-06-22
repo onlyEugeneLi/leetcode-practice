d702fc0c82bf72f8a574d00400dbe230f4a6d7df
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-06-22 17:06:12 +0100

    Add stack and slicing solutions

--- Array-List/1047-Remove-All-Adjacent-Duplicates-in-String.py
+++ Array-List/1047-Remove-All-Adjacent-Duplicates-in-String.py
@@ -30,3 +30,36 @@ class Solution_brute_force:
             # How to verify no more adjacent duplicates left? 
         return ''.join(s_array)
 
+class Solution_slicing:
+    def removeDuplicates(self, s: str) -> str:
+        # Base case
+        if len(s) == 1:
+            return s
+        
+        # string immutable -- convert to list so to remove elements | could also use slicing
+        
+        i = 0
+        while i < len(s) - 1: # Avoid 'out-of-bound' situation
+            # Identify duplicates and make sure index is within range
+            if i >= 0 and s[i] == s[i + 1]: 
+                # Remove duplicates
+                s = s[: i] + s[i + 2 : ]
+                i -= 1 # original index i character is removed so move back
+            else: 
+                # No duplicate identified, move to next
+                i += 1 
+
+        return s
+
+class Solution_stack:
+    def removeDuplicates(self, s: str) -> str:
+
+        string_stack = []
+
+        for char in s:
+            if string_stack and string_stack[-1] == char:
+                string_stack.pop() # Next one is duplicate, remove existing one
+            else:
+                string_stack.append(char)
+
+        return ''.join(string_stack)
