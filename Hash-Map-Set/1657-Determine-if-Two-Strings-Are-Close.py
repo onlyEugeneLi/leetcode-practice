6077f43f8ccbb26acc8534a456291d1b47ff8b18
onlyEugeneLi <69563490+onlyEugeneLi@users.noreply.github.com>
2025-06-23 10:34:27 +0100

    Add solution

--- Hash-Map-Set/1657-Determine-if-Two-Strings-Are-Close.py
+++ Hash-Map-Set/1657-Determine-if-Two-Strings-Are-Close.py
@@ -27,3 +27,35 @@ class Solution:
                 return False
             
         return True
+
+from collections import Counter
+class Solution_built_in_hash:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        # 2 strings
+        # All lowercase
+        # Length can be same or different
+        # Edge case
+        # Not same length, impossible to be close
+        if len(word1) != len(word2):
+            return False
+        # Same words
+        if word1 == word2:
+            return True
+        # Approach: Hash index map
+        # Requirement for close words:
+        # 1. Contain same characters
+        # 2. Contain same occurrence distribution
+
+        # Hashing
+        occurrence_word1 = Counter(word1) # {'a': occurrences, ...}
+        occurrence_word2 = Counter(word2)
+
+        # Number of occurences for each characters in both words
+        occurrence_value_word1 = sorted(occurrence_word1.values())
+        occurrence_value_word2 = sorted(occurrence_word2.values())
+        same_occurrence = occurrence_value_word1 == occurrence_value_word2
+
+        # Characters in both words: Are they same
+        same_character = occurrence_word1.keys() == occurrence_word2.keys()
+
+        return same_occurrence and same_character
